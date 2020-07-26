d25a04f8d7e8799efa45760a8a6b900e8d8e33c0
Mohammad Habib <mohmad.habib@gmail.com>
2020-07-26 22:10:12 +0300

    Update functions.php

--- web/functions.php
+++ web/functions.php
@@ -5,3 +5,11 @@ Vesta::add_header_menu('Web Apps', '/plugin/web-apps/');
 if (Vesta::is_plugin_page('web-apps')) {
     Vesta::add_css('/plugin/web-apps/style.css');
 }
+
+// Enqueue Ionicons.
+add_action( 'wp_enqueue_scripts', 'sp_enqueue_ionicons' );
+function sp_enqueue_ionicons() {
+
+	wp_enqueue_style( 'ionicons', '//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css', array(), CHILD_THEME_VERSION );
+
+}
